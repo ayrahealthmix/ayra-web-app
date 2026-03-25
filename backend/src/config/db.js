2ef8ab000727c636ac27687efc65505c6f90b069
lingaraj2002 <lingaraj01052002@gmail.com>
2026-03-25 12:14:17 +0530

    commit-check-mongo-url

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
+  console.log("MONGO_URL:", process.env.MONGO_URL);
   try {
     await mongoose.connect(process.env.MONGO_URL);
     console.log("MongoDB connected successfully");

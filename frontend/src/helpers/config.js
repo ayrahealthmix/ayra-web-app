cd1c20a4c70f0d159ebf7fed2abdd09647000028
lingaraj2002 <lingaraj01052002@gmail.com>
2026-04-10 00:23:52 +0530

    config-issue-fixed-10042026-1223

--- frontend/src/helpers/config.js
+++ frontend/src/helpers/config.js
@@ -1,14 +1,7 @@
 // Production environment
-// const BASE_URL = "http://www.ayrahealthmix.com";
-// const API_URL = "http://api.ayrahealthmix.com/api";
+// export const BASE_URL = "http://www.ayrahealthmix.com";
+// export const API_URL = "http://api.ayrahealthmix.com/api";
 
 // Development environment
-const BASE_URL = "http://localhost:5000";
-const API_URL = "http://localhost:5000/api";
-
-const config = {
-  BASE_URL: BASE_URL,
-  API_URL: API_URL,
-};
-
-export default config;
+export const BASE_URL = "http://localhost:5000";
+export const API_URL = "http://localhost:5000/api";

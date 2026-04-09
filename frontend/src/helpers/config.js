1103ad2bbe24eefccf750bca6c686c8edd408b8e
lingaraj2002 <lingaraj01052002@gmail.com>
2026-04-10 00:28:21 +0530

    api-url-cmded-10042026-1228

--- frontend/src/helpers/config.js
+++ frontend/src/helpers/config.js
@@ -1,7 +1,7 @@
 // Production environment
-// export const BASE_URL = "http://www.ayrahealthmix.com";
-// export const API_URL = "http://api.ayrahealthmix.com/api";
+export const BASE_URL = "http://www.ayrahealthmix.com";
+export const API_URL = "http://api.ayrahealthmix.com/api";
 
 // Development environment
-export const BASE_URL = "http://localhost:5000";
-export const API_URL = "http://localhost:5000/api";
+// export const BASE_URL = "http://localhost:5000";
+// export const API_URL = "http://localhost:5000/api";

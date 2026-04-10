063d731db4cc7fc797d340a2e3644af5252aa04e
lingaraj2002 <lingaraj01052002@gmail.com>
2026-04-10 22:47:29 +0530

    url-updated

--- frontend/src/helpers/config.js
+++ frontend/src/helpers/config.js
@@ -1,10 +1,10 @@
 // Development environment
-export const BASE_URL = "http://localhost:5000";
-export const API_URL = "http://localhost:5000/api";
+// export const BASE_URL = "http://localhost:5000";
+// export const API_URL = "http://localhost:5000/api";
 
 // Production environment
-// export const BASE_URL = "http://www.ayrahealthmix.com";
-// export const API_URL = "http://api.ayrahealthmix.com/api";
+export const BASE_URL = "http://www.ayrahealthmix.com";
+export const API_URL = "http://api.ayrahealthmix.com/api";
 export const EMAIL_ADDRESS = "ayrahealthmix@gmail.com";
 export const PHONE_NUMBER = "+919360988964";
 export const WHATSAPP_NUMBER = "+919360988964";
